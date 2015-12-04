2aca75d9e83024bd34630883d82c63e3a85f5d6c
Daniel <Daniel@Daniel>
2015-12-04 11:21:18 +0100

    Start

---
+++ src/y52/Y52.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package y52;
+
+import battleship.interfaces.BattleshipsPlayer;
+import tournament.player.PlayerFactory;
+
+/**
+ *
+ * @author Tobias Grundtvig
+ */
+public class Y52 implements PlayerFactory<BattleshipsPlayer>
+{
+
+    public Y52(){}
+    
+    
+    @Override
+    public BattleshipsPlayer getNewInstance()
+    {
+        return new RandomPlayer();
+    }
+
+    @Override
+    public String getID()
+    {
+        return "E1";
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Random player";
+    }
+    
+}
